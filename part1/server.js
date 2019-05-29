5762f659d08a0de14c510d04d5e34857fbc54920
zrhr <wilsondanielb@outlook.com>
2019-05-29 16:11:26 -0700

    Meow

--- part1/server.js
+++ part1/server.js
@@ -5,7 +5,22 @@ var port = process.env.PORT || 8000;
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
-
+app.get('/hello', (req, res)=>{
+  res.send("Hello!")
+})
+app.post('/create/:name', (req,res)=>{
+ res.json({"id":1, "name":req.params.name})
+})
+app.get('/',(req,res)=>{
+  res.sendFile("/index.html", {root: __dirname})
+})
+app.get('/verify/:age',(req,res)=>{
+  if(req.params.age>13)
+  res.sendStatus(200);
+  else {
+    res.sendStatus(403);
+  }
+})
 app.use(function(req, res) {
   res.sendStatus(404);
 });
--- part2/server.js
+++ part2/server.js
@@ -1,15 +1,50 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var fs = require("fs");
+var path = require("path");
 var port = process.env.PORT || 8000;
-
-app.get('/yourroute', function(req, res) {
+let tempArray = [];
+let temp2 = "";
+app.get("/yourroute", function(req, res) {
   res.send("stuff");
 });
+app.post("/create/:name/:age", (req, res) => {
+  let temporary = { name: req.params.name, age: req.params.age };
+  let temp = fs.readFileSync(path.join(__dirname, "/storage.json"), "utf-8");
+
+  if (temp.length === 0) {
+    tempArray.push(temporary);
+    temp2 = JSON.stringify(tempArray);
+  } else {
+    tempArray = JSON.parse(temp);
+    tempArray.push(temporary);
+    temp2 = JSON.stringify(tempArray);
+  }
+  console.log(temp2);
+  fs.writeFileSync(path.join(__dirname, "/storage.json"), temp2);
+
+  res.send("success");
+});
+app.get("/", function(req, res) {
+let temp = fs.readFileSync(path.join(__dirname, "/storage.json"), "utf-8");
+  res.send(temp);
+});
+app.get("/:name", function(req, res) {
+let temp = fs.readFileSync(path.join(__dirname, "/storage.json"), "utf-8");
+ tempArray = JSON.parse(temp);
+ for(let i=0; i<tempArray.length; i++)
+ {
+   if(req.params.name==tempArray[i].name)
+   res.send(JSON.stringify(tempArray[i]))
+ }
+ res.sendStatus(400)
+});
+
 
 app.use(function(req, res) {
   res.sendStatus(404);
 });
 
 app.listen(port, function() {
-  console.log('Listening on port', port);
+  console.log("Listening on port", port);
 });
